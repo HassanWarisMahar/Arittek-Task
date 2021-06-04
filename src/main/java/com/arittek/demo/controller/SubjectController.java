81d3de51cb75ec5c72b27872fd311ccf517f90e3
unknown <hassanwars36@gmail.com>
2021-06-03 17:55:03 -0700

    UI Updated

--- src/main/java/com/arittek/demo/controller/SubjectController.java
+++ src/main/java/com/arittek/demo/controller/SubjectController.java
@@ -11,13 +11,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
-@RestController
+@Controller
 @RequestMapping(value = "/subject")
 public class SubjectController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
